e8f39c324e1d7fe2b7f3b4923122e085a655a328
Omkar Kasture <kastureomkar4645@gmail.com>
2026-03-11 11:20:25 +0530

    my

---
+++ OMKAR/src/DEMO.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class DEMO {
+public static void main(String[] args) {
+	
+	String employee[]= {"shuhbh","om","ishhu"};
+	
+	for(int i =0;i<employee.length;i++) {
+		
+		System.out.print(employee[i]+" ");
+		
+	}
+	System.out.println();
+	System.out.println(employee[1]);
+	
+	
+	
+//	List l = new ArrayList();
+//	l.add("shubh");
+//	l.add("om");
+//	l.add("sachi");
+//	l.add("ishhu");
+//	
+//	System.out.println(l);
+}
+
+}
